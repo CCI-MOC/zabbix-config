965923c7fbab483b496537f5120c4827b29c606c
Naved Ansari <naved001@gmail.com>
2019-09-13 11:12:04 -0400

    Add a new alertscript to push alerts to rocketchat
    The instructions to use the alertscript are in the file description.

---
+++ alertscripts/rocketchat.py
@@ -0,0 +1,21 @@
+"""This script sends zabbix alerts to rocketchat.
+
+1. Place this script in /usr/lib/zabbix/alertscripts
+2. Give zabbix user permissions to execute the script.
+3. Create new "Media Type" under "Administration" Tab of type script.
+4. Add 3 script parameters: {ALERT.SENDTO}, {ALERT.SUBJECT} and {ALERT.MESSAGE}
+5. Set a user to use this Media Type and set it's sendto parameter the webhook
+for rocketchat.
+
+"""
+#! /usr/bin/python
+
+import requests
+import sys
+
+url = sys.argv[1]
+subject = ":bomb: " + sys.argv[2] + " :bomb:"
+message = sys.argv[3]
+
+payload = {"text": subject + "\n" + message + "\n"}
+r = requests.post(url, json=payload)
